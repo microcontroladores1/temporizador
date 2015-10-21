404822d75ba4abfbff72f9fb898fe0b14360880b
Rodrigues Filho <ednofco@gmail.com>
2015-10-21 16:31:02 -0300

    Adicionei as definicoes do projeto.
    Foram adiconada as definicoes do 89s52 e do LCD no temporizador.asm.
    O *.gitignore foi atualizado para nao versionar resultados de compilação.

--- temporizador.asm
+++ temporizador.asm
@@ -1,1 +1,23 @@
 ; Pratica de temporizador.
+; Programador: Fco Edno
+;
+; Temporizador para acionamento de lampada UV.
+
+;******************************************************************************
+; Diretivas ASSEMBLY
+;******************************************************************************
+$NOMOD51
+$INCLUDE (89S52.MCU)
+
+;******************************************************************************
+; Definicoes 
+;******************************************************************************
+LCD_data	equ		p2			; barramento de dados
+LCD_bf		bit		LCD_data.7	; busy flag
+LCD_rw		bit		p3.5		; read/write
+LCD_en		bit		p3.6		; pino de enable (clock)
+LCD_rs		bit		p3.7		; register select
+
+;******************************************************************************
+			end
+;******************************************************************************
