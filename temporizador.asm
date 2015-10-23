5f26bddbf736a60cdde0abf4d626c3bdea05c4e3
Rodrigues Filho <ednofco@gmail.com>
2015-10-23 18:33:31 -0300

    Esquematico
    Completei o esquematico da interface basica com LCD no projeto.
    Além disso, ajustei as variaveis LCD_rs, LCD_rw e LCD_en para ficar mais
    fácil a distribuição no hardware.

--- temporizador.asm
+++ temporizador.asm
@@ -14,9 +14,9 @@ $INCLUDE (89S52.MCU)
 ;******************************************************************************
 LCD_data	equ		p2			; barramento de dados
 LCD_bf		bit		LCD_data.7	; busy flag
-LCD_rw		bit		p3.5		; read/write
-LCD_en		bit		p3.6		; pino de enable (clock)
-LCD_rs		bit		p3.7		; register select
+LCD_rs		bit		p3.5		; register select
+LCD_rw		bit		p3.6		; read/write
+LCD_en		bit		p3.7		; pino de enable (clock)
 
 ;******************************************************************************
 ; MACROS
